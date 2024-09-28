c16df3587fce7600562f70a85cc6c3942708875f
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 17:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+function result(event) {
+    event.preventDefault();
+    let form = document.getElementById("form");
+    let arr = [];
+    for (let element of form.elements) {
+        if (element.type !== "submit") {
+            let label = document.querySelector(`label[for="${element.id}"]`);
+            if (label) {
+                arr.push(`${label.innerText} ${element.value}`);
+            } else {
+                arr.push(`No label for ${element.id} ${element.value}`);
+            }
+        }
+    }
+    alert(arr.join(" "));
+}
+
